75d31b6b0499f8816845cae66ed3a509bfab7a62
pmolina90 <P.a.molina90@gmail.com>
2025-01-14 16:35:03 -0500

    finalized changes and testing deployment

--- content-wheel-app/next.config.js
+++ content-wheel-app/next.config.js
@@ -2,10 +2,12 @@
 const nextConfig = {
   output: 'export', // Enables static file export
   trailingSlash: true, // Required for static hosting
-  basePath: 'content_wheel', // Leave empty for local testing, set for GitHub Pages later
+  assetPrefix: '/content_wheel', 
+  basePath: '/content_wheel', // Leave empty for local testing, set for GitHub Pages later
   images: {
     unoptimized: true, // Required for static hosting
   },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
