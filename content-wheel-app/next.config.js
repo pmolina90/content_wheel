7e353f0891b6a2058ba1c9350d5cf0cbcfc9270f
pmolina90 <P.a.molina90@gmail.com>
2025-01-14 11:53:30 -0500

    made changes to next.config

--- content-wheel-app/next.config.js
+++ content-wheel-app/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export', // Enables static file export
   trailingSlash: true, // Required for static hosting
-  basePath: '', // Leave empty for local testing, set for GitHub Pages later
+  basePath: 'content_wheel', // Leave empty for local testing, set for GitHub Pages later
   images: {
     unoptimized: true, // Required for static hosting
   },
